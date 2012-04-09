8462995dd3b13b3f8cf943821dfbdd6c45b921ce
Kelly Becker <kellylsbkr@gmail.com>
2012-04-09 10:03:02 -0600

    forgot to remove my ua

--- _bundle.php
+++ _bundle.php
@@ -143,8 +143,7 @@ class Bundle {
 		"YandexBot",
 		"yacybot",
 		"Yahoo-MMCrawler",
-		"yetibot",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19"
+		"yetibot"
 	);
 	
 	public function __construct($dir) {
